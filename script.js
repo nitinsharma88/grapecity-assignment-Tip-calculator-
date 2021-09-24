7ccd2262610d153e9976cda87c8dc9ea317add02
nitinsharma88 <74198110+nitinsharma88@users.noreply.github.com>
2021-09-24 15:43:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const form = document.getElementById('calculator')
+// form submit event handler
+form.onsubmit = function(event){
+  event.preventDefault()
+
+const howMuch = document.getElementById('bill')
+const tipAmount = document.getElementById('percentage')
+const customerNum = document.getElementById('customer-num')
+
+const totalBill = document.getElementById('total-bill')
+const totalPercentage = document.getElementById('total-percentage')
+const totalPerCustomer = document.getElementById('total-per-customer')
+
+  let cost = parseFloat(howMuch.value);
+  let customers = parseInt(customerNum.value);
+
+  let percentage = 0;
+  if(tipAmount.value !== ''){
+    percentage = parseInt(tipAmount.value)
+  }
+/*
+  console.log(cost);
+  console.log(percentage);
+  console.log(customers);
+  */
+
+//console.log('RESULTS');
+let percentageAmount = calculatePercentageAmount(cost, percentage);
+totalPercentage.value = percentageAmount
+//console.log('percentage Amount ' + percentageAmount);
+
+let totalValue = calculateTotalValue(cost, percentageAmount,customers);
+totalBill.value = totalValue
+//console.log('Total Value ' + totalValue);
+
+let totalPerPerson = calculatePerCustomer(percentageAmount, customers);
+totalPerCustomer.value= totalPerPerson
+///console.log('Total Per Customer:  ' + totalPerPerson);
+
+  return false
+}
+
+function calculatePercentageAmount(cost, percentage){
+  return Math.round(cost/100 * percentage)
+}
+
+
+function calculateTotalValue(cost, percentageAmount,customers) {
+  return Math.round((cost + percentageAmount)/customers)
+}
+
+function calculatePerCustomer(totalPerPerson, customers){
+  return (totalPerPerson / customers)
+}
